8b8ad9fd3a85ea32324543e0364485396da1d3fb
dylanonelson <dylanonelson@gmail.com>
2018-05-17 20:30:28 -0400

    Remove all moment locales from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,66 @@
-var config = require('./webpack.base.config.js');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = config;
+module.exports = {
+  devServer: {
+    contentBase: 'dist',
+    historyApiFallback: true,
+    inline: true,
+    port: 1104
+  },
+  devtool: 'source-map',
+  entry: {
+    index: './src/index.js',
+  },
+  mode: (process.env.NODE_ENV === 'production' ? 'production' : 'development'),
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: false,
+          },
+        }
+      ],
+    }, {
+      test: /\.html/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].html',
+        },
+      }],
+    }, {
+      enforce: 'pre',
+      include: /src/,
+      use: [{
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      }],
+      test: /\.jsx?$/,
+    }],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ],
+  }
+}
