f67e14a1ad6622afe08a42e1497d65e70138827c
dylanonelson <dylanonelson@gmail.com>
2018-05-19 22:48:03 -0400

    Clean up config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
-var config = require('./.eslintrc.base.js');
-
-module.exports = config;
+module.exports = {
+  'env': {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  'extends': ['eslint:recommended'],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 6,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'no-unused-vars': [2, { 'args': 'none' }],
+    'no-console': [
+      1,
+      {
+        'allow': ['debug', 'warn', 'error']
+      }
+    ],
+    'no-debugger': 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'sort-keys': [2, 'asc', { caseSensitive: true }],
+  }
+};
